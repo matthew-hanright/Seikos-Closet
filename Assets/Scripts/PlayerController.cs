dbd3e12c6fe2da28c0b8c4183332b4e67f9a12f3
Thinkpad <grnpndrs4@gmail.com>
2019-09-06 15:23:45 -0400

    First playercontroller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool isGrounded;
+    private float maxSpeed = 7;
+    private float takeOffSpeed = 7;
+    private Vector2 move;
+    public GameObject player;
+    private float distToGround;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = this.gameObject;
+        distToGround = player.GetComponent<BoxCollider2D>().bounds.extents.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move.x = Input.GetAxis("Horizontal");
+        player.transform.Translate(move);
+    }
+}
