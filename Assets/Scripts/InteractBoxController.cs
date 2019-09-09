b6c47e84765a06f86bb6df30b776ac828d22f84c
Snorlax9 <grnpndrs4@gmail.com>
2019-09-08 23:16:49 -0400

    Began dialogue system, added interact box and controller

---
+++ Assets/Scripts/InteractBoxController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractBoxController : MonoBehaviour
+{
+    private bool canInteract = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Interact") <= 0.1)
+        {
+            canInteract = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (canInteract && other.GetComponent<Interactable>())
+        {
+            if (Input.GetAxis("Interact") > 0.5)
+            {
+                Debug.Log("High");
+            }
+            canInteract = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private bool isGrounded;
-    private float maxSpeed = 7;
-    private float takeOffSpeed = 7;
-    private Vector2 move;
     public GameObject player;
+    public int maxSpeed = 7;
+    public int takeOffSpeed = 7;
+
+    private bool isGrounded;
+    private Vector2 move;
     private float distToGround;
     // Start is called before the first frame update
     void Start()
@@ -20,7 +21,35 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Interactions
+
+        //Movement
         move.x = Input.GetAxis("Horizontal");
+        if(move.x > maxSpeed)
+        {
+            move.x = maxSpeed;
+        }
+        if (move.x < 0)
+        {
+            if (transform.localScale.x > 0)
+            {
+                transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y);
+                player.GetComponentInChildren<BoxCollider2D>().offset = new Vector2(
+                    player.GetComponentInChildren<BoxCollider2D>().offset.x * -1,
+                    player.GetComponentInChildren<BoxCollider2D>().offset.y);
+            }
+        }
+        else if (move.x > 0)
+        {
+            if (transform.localScale.x < 0)
+            {
+                transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y);
+                player.GetComponentInChildren<BoxCollider2D>().offset = new Vector2(
+                    player.GetComponentInChildren<BoxCollider2D>().offset.x * -1,
+                    player.GetComponentInChildren<BoxCollider2D>().offset.y);
+            }
+        }
         player.transform.Translate(move);
     }
+
 }
